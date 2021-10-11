5f3a07ae4a4d1a8854b8a195fc8f83005b09b692
Tasdeed Aziz <ayonaziz@gmail.com>
2021-10-11 18:05:40 -0400

    header updates

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../../Component/Header/Header.scss";
 
 function Header() {
   return (
     <div className="header">
-      <h1>PrepTube</h1>
+      <Link className="homepage" to="/">
+        <h1>PrepTube</h1>
+      </Link>
       {/* <img className="header__logo" src={playLogo} alt='logo'/> */}
     </div>
   );
