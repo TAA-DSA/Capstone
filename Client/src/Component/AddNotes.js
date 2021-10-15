ac58b59c8c98e075de2d80ff3bbcef644a09290f
Tasdeed Aziz <ayonaziz@gmail.com>
2021-10-15 13:46:53 -0400

    addNores updates

--- Client/src/Component/AddNotes.js
+++ Client/src/Component/AddNotes.js
@@ -1,37 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import "../Component/MainVideo/MainVideo.scss";
-import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-function AddNotes() {
-  //const [noteText, setNoteText] = useState("");
-
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    //setNoteText(event.target.value);
+class AddNotes extends Component {
+  state = {
+    note: "",
   };
 
-  const handleSubmit = (event) => {
+  handleChange = (event) => {
+    this.setState({
+      [event.target.note]: event.target.value,
+    });
+    console.log(event.target.input);
+  };
+
+  handleSubmit = (event) => {
     event.preventDefault();
+    axios.post(`http://localhost:8080/notes`, this.state).then((addVideo) => {
+      // console.log(addVideo);
+      // console.log("id", addVideo.data.id);
+      this.setState({
+        note: "",
+      });
+    });
   };
 
-  return (
-    <div>
-      <textarea
-        className="main__notebox"
-        input="text"
-        placeholder="Make Notes here..."
-        onChange={handleChange}
-      />
+  render() {
+    return (
+      <div>
+        <textarea
+          className="main__notebox"
+          input="text"
+          placeholder="Make Notes here..."
+          onChange={this.handleChange}
+        />
 
-      <div className="main__button">
-        <button className="main__button__save">Save</button>
-        <Link className="main__button__link" to="/notes">
-          <button className="main__button__save">Notes</button>
-        </Link>
+        <div className="main__button">
+          <button className="main__button__save" onClick={this.handleSubmit}>
+            Save
+          </button>
+          <Link className="main__button__link" to="/notes">
+            <button className="main__button__save">Notes</button>
+          </Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default AddNotes;
